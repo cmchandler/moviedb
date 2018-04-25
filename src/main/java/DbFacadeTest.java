d198ffb51d723b10fdb4827cb05864f6f553b65d
nisule <nicksundvall@gmail.com>
2018-04-25 11:17:26 -0700

    Update DbFacadeTest.java

--- src/main/java/DbFacadeTest.java
+++ src/main/java/DbFacadeTest.java
@@ -46,115 +46,149 @@ public class DbFacadeTest {
 
 		}
 	}
-	
-	
-	/*
-	EVERYTHING BELOW THIS IS EXAMPLES
-	*/
 
-	@Test 
-	public void testGetNameSalaryResearch() throws SQLException {
-		List<String> lastNames = Arrays.asList( "English", "Narayan", "Smith", "Wong" );
-		List<String> firstNames = Arrays.asList( "Joyce", "Ramesh", "John", "Franklin" );
-		List<String> salaries = Arrays.asList( "25000.00", "38000.00", "30000.00", "40000.00" );
+	@Test
+	public void testGrantAdministrator() throws SQLException {
+		ArrayList<String> username = new ArrayList<>(Arrays.asList("user1"));
 
 		try( DbFacade db = new DbFacade() ) {
-			ResultSet rset = db.getNameSalaryResearch();
+			ResultSet rset = db.grantAdministrator("user1");
 
 			// Use a loop to read all rows
-			List<String> actualFirstNames = new ArrayList<>();
-			List<String> actualLastNames = new ArrayList<>();
-			List<String> actualSalaries = new ArrayList<>();
+			List<String> actualUsername = new ArrayList<>();
 			while( rset.next() ) {
-				actualLastNames.add(rset.getString(1) );
-				actualFirstNames.add(rset.getString(2) );
-				actualSalaries.add(rset.getString(3) );
+				actualUsername.add(rset.getString(1) );
 			}
 
 			// Compare the lists to the expected results
-			assertEquals(salaries, actualSalaries);
-			assertEquals(lastNames, actualLastNames);
-			assertEquals(firstNames, actualFirstNames);
-		}
-	}
+			assertTrue(actualUsername.contains(username.get(0)));
 
-	@Test 
-	public void testLastNameStartsWithW() throws SQLException {
-		List<String> lastNames = Arrays.asList( "Wallace", "Wong", "Wallis" );
-		List<String> firstNames = Arrays.asList( "Jennifer", "Franklin", "Evan" );
-		List<Integer> dnos = Arrays.asList( 4,5,7 );
-
-		try( DbFacade db = new DbFacade() ) {
-			ResultSet rset = db.lastNameStartsWithW();
-
-			// Use a loop to read all rows
-			List<String> actualFirstNames = new ArrayList<>();
-			List<String> actualLastNames = new ArrayList<>();
-			List<Integer> actualDnos = new ArrayList<>();
-			while( rset.next() ) {
-				actualDnos.add(rset.getInt(1) );
-				actualLastNames.add(rset.getString(2) );
-				actualFirstNames.add( rset.getString(3) );
-			}
-
-			// Compare the lists to the expected results
-			assertEquals(dnos, actualDnos);
-			assertEquals(lastNames, actualLastNames);
-			assertEquals(firstNames, actualFirstNames);
-		}
-	}
-	
-	@Test 
-	public void testEmployeesByDno() throws SQLException {
-		List<String> lastNames = Arrays.asList("Jabbar", "Wallace", "Zelaya");
-		List<String> firstNames = Arrays.asList("Ahmad", "Jennifer", "Alicia");
-
-		try( DbFacade db = new DbFacade() ) {
-			ResultSet rset = db.employeesByDNO(4);
-
-			// Use a loop to read all rows and store in lists
-			ArrayList<String> actualFirstNames = new ArrayList<>();
-			ArrayList<String> actualLastNames = new ArrayList<>();
-			while( rset.next() ) {
-				actualLastNames.add(rset.getString(1));
-				actualFirstNames.add(rset.getString(2));
-			}
-			
-			// Compare the lists to the expected results
-			assertEquals(lastNames, actualLastNames);
-			assertEquals(firstNames, actualFirstNames);
 		}
 	}
 
 	@Test
-	public void testNameProjectHours() throws SQLException {
-	    // need to populate lists with correct data!! this is for comparison
-		List<String> lastNames = Arrays.asList();
-        List<String> firstNames = Arrays.asList();
-        List<String> projectNumber = Arrays.asList();
-        List<String> hours = Arrays.asList();
+	public void testGrantCritic() throws SQLException {
+		ArrayList<String> username = new ArrayList<>(Arrays.asList("cbryers12"));
 
-        try( DbFacade db = new DbFacade() ) {
-            ResultSet rset = db.nameProjectHours("1");
+		try( DbFacade db = new DbFacade() ) {
+			ResultSet rset = db.grantAdministrator("cbryers12");
 
-            // Use a loop to read all rows and store in lists
-            ArrayList<String> actualFirstNames = new ArrayList<>();
-            ArrayList<String> actualLastNames = new ArrayList<>();
-            ArrayList<String> actualProjectNumber = new ArrayList<>();
-            ArrayList<String> actualHours = new ArrayList<>();
+			// Use a loop to read all rows
+			List<String> actualUsername = new ArrayList<>();
+			while( rset.next() ) {
+				actualUsername.add(rset.getString(1) );
+			}
 
-            while( rset.next() ) {
-                actualLastNames.add(rset.getString(1));
-                actualFirstNames.add(rset.getString(2));
-                actualProjectNumber.add(rset.getString(3));
-                actualHours.add(rset.getString(4));
-            }
+			// Compare the lists to the expected results
+			assertTrue(actualUsername.contains(username.get(0)));
 
-            // Compare the lists to the expected results
-            assertEquals(lastNames, actualLastNames);
-            assertEquals(firstNames, actualFirstNames);
-            assertEquals(projectNumber, actualProjectNumber);
-            assertEquals(hours, actualHours);
-        }
+		}
 	}
+
+	// not sure about this one or what strings are added in the addComment method
+	@Test
+	public void testAddComment() throws SQLException {
+		ArrayList<String> content = new ArrayList<>(Arrays.asList("whatever the comment is here"));
+
+		try( DbFacade db = new DbFacade() ) {
+			ResultSet rset = db.addComment("whatever the comment is here");
+
+			// Use a loop to read all rows
+			List<String> actualContent = new ArrayList<>();
+			while( rset.next() ) {
+				actualContent.add(rset.getString(2) );
+			}
+
+			// Compare the lists to the expected results
+			assertTrue(actualContent.contains(content.get(0)));
+
+		}
+	}
+
+	@Test
+	public void testDeleteComment() throws SQLException {
+		ArrayList<String> comment_id = new ArrayList<>(Arrays.asList("1"));
+
+		try( DbFacade db = new DbFacade() ) {
+			ResultSet rset = db.deleteComment("1");
+
+			// Use a loop to read all rows
+			List<String> actualComment_id = new ArrayList<>();
+			while( rset.next() ) {
+				actualComment_id.add(rset.getString(1) );
+			}
+
+			// Compare the lists to the expected results
+			// make sure the actual doesn't contain that id because it got removed
+			assertTrue(!actualComment_id.contains(comment_id.get(0)));
+
+		}
+	}
+
+	@Test
+	public void testPostReview() throws SQLException {
+		ArrayList<String> movie = new ArrayList<>(Arrays.asList("movie"));
+		ArrayList<String> image = new ArrayList<>(Arrays.asList("image"));
+		ArrayList<String> rating = new ArrayList<>(Arrays.asList("rating"));
+
+		try( DbFacade db = new DbFacade() ) {
+			ResultSet rset = db.postReview("movie", "image", "rating");
+
+			// Use a loop to read all rows
+			List<String> actualMovie = new ArrayList<>();
+			List<String> actualImage = new ArrayList<>();
+			List<String> actualRating = new ArrayList<>();
+			while( rset.next() ) {
+				actualMovie.add(rset.getString(2) );
+				actualImage.add(rset.getString(3) );
+				actualRating.add(rset.getString(4) );
+			}
+
+			// Compare the lists to the expected results
+			assertTrue(actualMovie.contains(movie.get(0)));
+			assertTrue(actualImage.contains(movie.get(0)));
+			assertTrue(actualRating.contains(movie.get(0)));
+
+		}
+	}
+
+	@Test
+	public void testListMovieReviews() throws SQLException {
+		ArrayList<String> movies = new ArrayList<>(Arrays.asList("movie"));
+
+		try( DbFacade db = new DbFacade() ) {
+			ResultSet rset = db.listMovieReviews("movie");
+
+			// Use a loop to read all rows
+			List<String> actualMovies = new ArrayList<>();
+			while( rset.next() ) {
+				actualMovies.add(rset.getString(1) );
+			}
+
+			// Compare the lists to the expected results
+			assertTrue(actualMovies.contains(movies.get(0)));
+
+		}
+	}
+
+	@Test
+	public void testDeleteUser() throws SQLException {
+		ArrayList<String> username = new ArrayList<>(Arrays.asList("user1"));
+
+		try( DbFacade db = new DbFacade() ) {
+			ResultSet rset = db.deleteUser("user1");
+
+			// Use a loop to read all rows
+			List<String> actualUsername = new ArrayList<>();
+			while( rset.next() ) {
+				actualUsername.add(rset.getString(1) );
+			}
+
+			// Compare the lists to the expected results
+			// actualUser doesn't contain username
+			assertTrue(!actualUsername.contains(username.get(0)));
+
+		}
+	}
+	
 }
