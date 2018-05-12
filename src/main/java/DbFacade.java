b4c62a83703fbd4ab0de13b69d1461fe66098618
nisule <nicksundvall@gmail.com>
2018-05-12 10:07:48 -0700

    Update DbFacade.java

--- src/main/java/DbFacade.java
+++ src/main/java/DbFacade.java
@@ -1,9 +1,16 @@
 /**
- * Prov
+ * This class provides an interface (Facade) for a database.  It should be used in the 
+ * following way:
+ * 
+ * try( DbFacade db = new DbFacade() ) {
+ * 	  // Use db to execute appropriate methods.  The close method will automatically 
+ *    // be called when we exit the try block.
+ * }
+ * 
  */
 
+// Import the java.sql package to use JDBC methods and classes
 import javax.imageio.ImageIO;
-import javax.sound.midi.Soundbank;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -14,45 +21,69 @@ import java.sql.*;
 
 public class DbFacade implements AutoCloseable {
 
-    private Connection conn;
+	private Connection conn = null; // Connection object
 
-    /**
-     * Constructor. Opens connection to DBMS with the username and password hardcoded here.
-     * @throws SQLException
-     */
-    public DbFacade() throws SQLException {
-        String url = "jdbc:mariadb://mal.cs.plu.edu:3306/367_2018_company";
-        String username = "cyan_2018";
-        String password = "367rocks!";
+	/**
+	 * The default constructor will load and register the MySQL driver
+	 */
+	public DbFacade() throws SQLException {
+		openDB();
+	}
 
-        conn = DriverManager.getConnection(url, username, password);
+	/**
+	 * @return the conn
+	 */
+	public Connection getConn() {
+		return conn;
+	}
 
-        System.out.println("Connection opened.");
-    }
+	/**
+	 * Open a MySQL DB connection where user name and password are predefined
+	 * (hardwired) within the method.
+	 */
+	private void openDB() throws SQLException {
+		// Connect to the database
+		String url = "jdbc:mysql://mal.cs.plu.edu:3306/367_2018_cyan";
+		String username = "cyan_2018";
+		String password = "367rocks!";
 
-    /**
-    * Closes the connection to the DBMS. Prints message to terminal if the closing operation was
-     * successful.
-     */
-    @Override
-    public void close() throws SQLException {
-        if( conn != null )
-            conn.close();
-        conn = null;
+		conn = DriverManager.getConnection(url, username, password);
+	}
 
-        System.out.println("Connection closed.");
+	/**
+	 * Close the connection to the DB
+	 */
+	public void close() {
+		try {
+			if(conn != null) conn.close();
+		} catch (SQLException e) {
+			System.err.println("Failed to close database connection: " + e);
+		}
+		conn = null;
+	}
 
-    }
+	public boolean authenticateUser( String username, String password ) throws SQLException {
+		String sql = "SELECT username FROM user WHERE " +
+				" username = ? AND " +
+				" password_hash = SHA2(?, 256)";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.clearParameters();
+		pstmt.setString(1, username);
+		pstmt.setString(2, password);
+		ResultSet rset = pstmt.executeQuery();
+		return rset.first();
+	}
 
-    /**
-     * Creates a new account and places the new tuple with the parameters passed into the
-     * database.
-     * @param username The desired username for the new user
-     * @param password The desired password for the new user
-     * @param email The user's email address
-     * @return ResultSet containing the created row
-     */
-    public ResultSet accountCreation(String username, String password, String email) {
+
+	/**
+	 * Creates a new account and places the new tuple with the parameters passed into the
+	 * database.
+	 * @param username The desired username for the new user
+	 * @param password The desired password for the new user
+	 * @param email The user's email address
+	 * @return ResultSet containing the created row
+	 */
+	public ResultSet accountCreation(String username, String password, String email) {
 		ResultSet rset = null;
 		String sql = null;
 
@@ -75,11 +106,11 @@ public class DbFacade implements AutoCloseable {
 		return rset;
 	}
 
-    /**
-     * Adds administrator permissions to an account.
-     * @param username The username associated with the account to grant Administrator
-     * @return ResultSet containing the modified row
-     */
+	/**
+	 * Adds administrator permissions to an account.
+	 * @param username The username associated with the account to grant Administrator
+	 * @return ResultSet containing the modified row
+	 */
 	public ResultSet grantAdministrator(String username) {
 		ResultSet rset = null;
 		String sql = null;
@@ -101,11 +132,11 @@ public class DbFacade implements AutoCloseable {
 		return rset;
 	}
 
-    /**
-     * Adds Critic permissions to an account.
-     * @param username The username associated with the account to grant Critic
-     * @return the modified row
-     */
+	/**
+	 * Adds Critic permissions to an account.
+	 * @param username The username associated with the account to grant Critic
+	 * @return the modified row
+	 */
 	public ResultSet grantCritic(String username) {
 		ResultSet rset = null;
 		String sql = null;
@@ -127,200 +158,199 @@ public class DbFacade implements AutoCloseable {
 		return rset;
 	}
 
-    /**
-     * Adds a comment.
-     * @param content The comment to add.
-     * @return the row containing the added comment.
-     */
+	/**
+	 * Adds a comment.
+	 * @param content The comment to add.
+	 * @return the row containing the added comment.
+	 */
 	public ResultSet addComment(String content) {
-        ResultSet rset = null;
-        String sql = null;
+		ResultSet rset = null;
+		String sql = null;
 
-        try {
-            // create a Statement and an SQL string for the statement
+		try {
+			// create a Statement and an SQL string for the statement
 
-            sql = "INSERT INTO comment VALUES(comment_id, ?, false, review, post_date, user)";
-            PreparedStatement pstmt = conn.prepareStatement(sql);
+			sql = "INSERT INTO comment VALUES(comment_id, ?, false, review, post_date, user)";
+			PreparedStatement pstmt = conn.prepareStatement(sql);
 
-            pstmt.clearParameters();
-            pstmt.setString(1, content); // set the 1 parameter
+			pstmt.clearParameters();
+			pstmt.setString(1, content); // set the 1 parameter
 
-            rset = pstmt.executeQuery();
-        } catch (SQLException e) {
-            System.out.println("addComment failed: " + e.getMessage());
-        }
+			rset = pstmt.executeQuery();
+		} catch (SQLException e) {
+			System.out.println("addComment failed: " + e.getMessage());
+		}
 
-        return rset;
-    }
+		return rset;
+	}
 
-    /**
-     * Deletes a comment with the provided comment ID.
-     * @param comment_id the id of the comment to delete
-     * @return the deleted row.
-     */
+	/**
+	 * Deletes a comment with the provided comment ID.
+	 * @param comment_id the id of the comment to delete
+	 * @return the deleted row.
+	 */
 	public ResultSet deleteComment(String comment_id) {
-        ResultSet rset = null;
-        String sql = null;
+		ResultSet rset = null;
+		String sql = null;
 
-        try {
-            // create a Statement and an SQL string for the statement
+		try {
+			// create a Statement and an SQL string for the statement
 
-            sql = "DELETE comment_id FROM comment WHERE comment_id=?";
-            PreparedStatement pstmt = conn.prepareStatement(sql);
+			sql = "DELETE comment_id FROM comment WHERE comment_id=?";
+			PreparedStatement pstmt = conn.prepareStatement(sql);
 
-            pstmt.clearParameters();
-            pstmt.setString(1, comment_id); // set the 1 parameter
+			pstmt.clearParameters();
+			pstmt.setString(1, comment_id); // set the 1 parameter
 
-            rset = pstmt.executeQuery();
-        } catch (SQLException e) {
-            System.out.println("deleteComment failed: " + e.getMessage());
-        }
+			rset = pstmt.executeQuery();
+		} catch (SQLException e) {
+			System.out.println("deleteComment failed: " + e.getMessage());
+		}
 
-        return rset;
-    }
+		return rset;
+	}
 
-    /**
-     * Adds a new review to the database, taking information from user.
-     *
-     * Image is currently in database as a BLOB, but the intent is to
-     * store the name of the image file, kept outside the database on the
-     * server.
-     *
-     * Image filename format: XXXXXX_YYYYYYYYYYYYYYYY
-     *
-     * Where XXXX = username
-     *       YYYYYYYYYYYYYY = hash of the image data from hashImage()
-     *
-     * @param movie The name of the movie the review is about
-     * @param image An image associated with the movie
-     * @param rating The rating of the movie given by the reviewer
-     * @return the created review row
-     */
-    public ResultSet postReview(String movie, String image, String rating) {
-        ResultSet rset = null;
-        String sql = null;
+	/**
+	 * Adds a new review to the database, taking information from user.
+	 *
+	 * Image is currently in database as a BLOB, but the intent is to
+	 * store the name of the image file, kept outside the database on the
+	 * server.
+	 *
+	 * Image filename format: XXXXXX_YYYYYYYYYYYYYYYY
+	 *
+	 * Where XXXX = username
+	 *       YYYYYYYYYYYYYY = hash of the image data from hashImage()
+	 *
+	 * @param movie The name of the movie the review is about
+	 * @param image An image associated with the movie
+	 * @param rating The rating of the movie given by the reviewer
+	 * @return the created review row
+	 */
+	public ResultSet postReview(String movie, String image, String rating) {
+		ResultSet rset = null;
+		String sql = null;
 
-        try {
-            // create a Statement and an SQL string for the statement
+		try {
+			// create a Statement and an SQL string for the statement
 
-            sql = "INSERT INTO review VALUES(review_id, ?, ?, ?, post_date, critic)";
-            PreparedStatement pstmt = conn.prepareStatement(sql);
+			sql = "INSERT INTO review VALUES(review_id, ?, ?, ?, post_date, critic)";
+			PreparedStatement pstmt = conn.prepareStatement(sql);
 
-            pstmt.clearParameters();
-            pstmt.setString(1, movie); // set the 1 parameter
-            pstmt.setString(1, image); // set the 2 parameter
-            pstmt.setString(1, rating); // set the 3 parameter
+			pstmt.clearParameters();
+			pstmt.setString(1, movie); // set the 1 parameter
+			pstmt.setString(1, image); // set the 2 parameter
+			pstmt.setString(1, rating); // set the 3 parameter
 
-            rset = pstmt.executeQuery();
-        } catch (SQLException e) {
-            System.out.println("postReview failed: " + e.getMessage());
-        }
+			rset = pstmt.executeQuery();
+		} catch (SQLException e) {
+			System.out.println("postReview failed: " + e.getMessage());
+		}
 
-        return rset;
-    }
+		return rset;
+	}
 
-    /**
-     * Gets a ResultSet containing all of the movie reviews related to a movie with
-     * a provided title.
-     * @param movie The name of the movie to search for reviews for
-     * @return ResultSet containing all of the associated rows with the title
-     */
-    public ResultSet listMovieReviews(String movie) {
-        ResultSet rset = null;
-        String sql = null;
+	/**
+	 * Gets a ResultSet containing all of the movie reviews related to a movie with
+	 * a provided title.
+	 * @param movie The name of the movie to search for reviews for
+	 * @return ResultSet containing all of the associated rows with the title
+	 */
+	public ResultSet listMovieReviews(String movie) {
+		ResultSet rset = null;
+		String sql = null;
 
-        try {
-            // create a Statement and an SQL string for the statement
+		try {
+			// create a Statement and an SQL string for the statement
 
-            sql = "SELECT post_date, movie, rating FROM review WHERE movie = ?";
-            PreparedStatement pstmt = conn.prepareStatement(sql);
+			sql = "SELECT post_date, movie, rating FROM review WHERE movie = ?";
+			PreparedStatement pstmt = conn.prepareStatement(sql);
 
-            pstmt.clearParameters();
+			pstmt.clearParameters();
 
-            pstmt.setString(1, movie); // set the 1 parameter
+			pstmt.setString(1, movie); // set the 1 parameter
 
-            rset = pstmt.executeQuery();
+			rset = pstmt.executeQuery();
 
-        } catch (SQLException e) {
-            System.out.println("listMovieReview failed: " + e.getMessage());
-        }
+		} catch (SQLException e) {
+			System.out.println("listMovieReview failed: " + e.getMessage());
+		}
 
-        return rset;
-    }
+		return rset;
+	}
 
-    /**
-     * Deletes a user with given username from the database.
-     * @param username the username of the user to delete.
-     * @return a row containing the deleted user
-     */
-    public ResultSet deleteUser(String username) {
-        ResultSet rset = null;
-        String sql = null;
+	/**
+	 * Deletes a user with given username from the database.
+	 * @param username the username of the user to delete.
+	 * @return a row containing the deleted user
+	 */
+	public ResultSet deleteUser(String username) {
+		ResultSet rset = null;
+		String sql = null;
 
-        try {
-            // create a Statement and an SQL string for the statement
+		try {
+			// create a Statement and an SQL string for the statement
 
-            sql = "DELETE username FROM user WHERE username=?";
-            PreparedStatement pstmt = conn.prepareStatement(sql);
+			sql = "DELETE username FROM user WHERE username=?";
+			PreparedStatement pstmt = conn.prepareStatement(sql);
 
-            pstmt.clearParameters();
-            pstmt.setString(1, username); // set the 1 parameter
+			pstmt.clearParameters();
+			pstmt.setString(1, username); // set the 1 parameter
 
-            rset = pstmt.executeQuery();
-        } catch (SQLException e) {
-            System.out.println("deleteUser failed: " + e.getMessage());
-        }
+			rset = pstmt.executeQuery();
+		} catch (SQLException e) {
+			System.out.println("deleteUser failed: " + e.getMessage());
+		}
 
-        return rset;
-    }
+		return rset;
+	}
 
-    /**
-     * Hashes an image to an MD5 hash.
-     * @param path the path to the image file to hash.
-     * @return a hash string unique to the image.
-     */
-    public static String hashImage(String path) {
+	/**
+	 * Hashes an image to an MD5 hash.
+	 * @param path the path to the image file to hash.
+	 * @return a hash string unique to the image.
+	 */
+	public static String hashImage(String path) {
 
-        try {
-            // Open file as BufferedImage
-            File input = new File(path);
-            BufferedImage image =ImageIO.read(input);
+		try {
+			// Open file as BufferedImage
+			File input = new File(path);
+			BufferedImage image = ImageIO.read(input);
 
-            // Convert image to bytearray
-            ByteArrayOutputStream os = new ByteArrayOutputStream();
-            ImageIO.write(image, "jpg", os);
-            byte[] byteArray = os.toByteArray();
+			// Convert image to bytearray
+			ByteArrayOutputStream os = new ByteArrayOutputStream();
+			ImageIO.write(image, "jpg", os);
+			byte[] byteArray = os.toByteArray();
 
-            // Hash bytearray
-            MessageDigest m = MessageDigest.getInstance("MD5");
-            m.update(byteArray);
-            byte[] hash = m.digest();
+			// Hash bytearray
+			MessageDigest m = MessageDigest.getInstance("MD5");
+			m.update(byteArray);
+			byte[] hash = m.digest();
 
-            return returnHex(hash);
+			return returnHex(hash);
 
 
-        } catch (IOException e) {
-            System.out.println("Error getting image hash. IO exception.");
-        } catch (NoSuchAlgorithmException e) {
-            System.out.println("Error finding hash algorithm. NoSuchAlgorithm exception.");
-        }
-        return "ERROR";
-    }
+		} catch (IOException e) {
+			System.out.println("Error getting image hash. IO exception.");
+		} catch (NoSuchAlgorithmException e) {
+			System.out.println("Error finding hash algorithm. NoSuchAlgorithm exception.");
+		}
+		return "ERROR";
+	}
 
-    /**
-     * Converts a byte array to a hex string.
-     * From http://www.rgagnon.com/javadetails/java-0596.html
-     * @param inBytes
-     * @return a string containing the hash.
-     */
-    private static String returnHex(byte[] inBytes)  {
-        String hexString = "";
-        for (int i=0; i < inBytes.length; i++) {
-            hexString +=
-                    Integer.toString( ( inBytes[i] & 0xff ) + 0x100, 16).substring( 1 );
-        }
-        return hexString;
-    }
-
+	/**
+	 * Converts a byte array to a hex string.
+	 * From http://www.rgagnon.com/javadetails/java-0596.html
+	 * @param inBytes
+	 * @return a string containing the hash.
+	 */
+	private static String returnHex(byte[] inBytes)  {
+		String hexString = "";
+		for (int i=0; i < inBytes.length; i++) {
+			hexString +=
+					Integer.toString( ( inBytes[i] & 0xff ) + 0x100, 16).substring( 1 );
+		}
+		return hexString;
+	}
 
 }
