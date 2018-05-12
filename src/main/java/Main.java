bd009d066f81f2bd680ec7162f8e8f22d959c334
nisule <nicksundvall@gmail.com>
2018-05-12 10:06:53 -0700

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,34 @@
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.Spark.post;
+
 public class Main {
+
     public static void main(String[] args) {
+        // Create the controller
+        ExampleController controller = new ExampleController();
+
+        // Configure routes.  This defines each URL and associates it with a controller method.
+        // Use get to setup methods for GET requests, and post to setup methods for POST requests.
+        get("/login", controller::getLoginForm);
+        post( "/authenticate", controller::postLoginForm);
+        get("/signUp", controller::signUp);
+        post("/signUpSuccess", controller::signUpSuccess);
+        get("/user/home", controller::userHome);
+        get("/admin/home", controller::adminHome);
+
+        Spark.before("/user/*", controller::userBefore);
+        Spark.before("/admin/*", controller::adminBefore);
 
     }
+
+    public static String renderTemplate(Map<String,Object> model, String path ) {
+        return new HandlebarsTemplateEngine().render(new ModelAndView(model, path));
+    }
+
 }
